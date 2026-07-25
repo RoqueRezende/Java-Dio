d4e1d94160fd51f1b5e8363b8438fd64e104fa41
RoqueRezende <gabriel1320nunes@gmail.com>
2026-07-24 22:09:50 -0300

    commit - Controle de Fluxo

---
+++ POO/Projetos/src/Classes/DesafioControleFluxo/ParametrosInvalidosException.java
@@ -0,0 +1,9 @@
+package Classes.DesafioControleFluxo;
+
+public class ParametrosInvalidosException extends Exception {
+
+    public ParametrosInvalidosException() {
+        super("O segundo parâmetro deve ser maior que o primeiro");
+    }
+
+}
